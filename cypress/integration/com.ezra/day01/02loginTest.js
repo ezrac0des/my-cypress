cfb608a9240a0b0c6f42c0d5ee5f999f9275be31
ezrac0des <ezraapaydin@gmail.com>
2022-08-08 18:31:05 -0400

    end

--- cypress/integration/com.ezra/day01/02loginTest.js
+++ cypress/integration/com.ezra/day01/02loginTest.js
@@ -4,11 +4,11 @@ describe("Crystal Key hotel", () => {
 
     //login
     //1. way: using open selector playground on the test page
-    //* get(): gets an element using css selector
+    //* get() -> gets an element using css selector
     cy.get("#navLogon > .nav-link").click();
 
     //2. way: using text attribute of the element
-    //* contains(): gets an element using text attribute
+    //* contains() -> gets an element using text attribute
     cy.contains("Log in").click();
   });
 });
--- cypress/integration/com.ezra/day02/02navigation.js
+++ cypress/integration/com.ezra/day02/02navigation.js
@@ -1,5 +1,47 @@
-describe('Navigation', () => { 
-    it('back, forward, refresh', ()=>{
-        cy.visit()
-    })
- })
+describe("Navigation", () => {
+    
+  //it.skip -> skips the test, doesnt execute it
+  it.skip("back, forward, refresh", () => {
+    cy.visit("https://qa-environment.koalaresorthotels.com/");
+    cy.contains("Log in").click();
+
+    //* wait(int) -> waits as given milliseconds (hard wait)
+    cy.wait(2000);
+
+    //* go('back') -> goes to previous page
+    cy.go("back");
+    cy.wait(2000);
+
+    //* go('forward') -> goes to next page
+    cy.go("forward");
+    cy.wait(2000);
+
+    //* go(-1) -> goes to previous page
+    cy.go(-1);
+    cy.wait(2000);
+
+    //* go(1) -> goes to next page
+    cy.go(1);
+    cy.wait(2000);
+
+    //* reload() -> reloads the page
+    cy.reload();
+    cy.wait(2000);
+  });
+
+  it("chained navigation", () => {
+    cy.visit("https://qa-environment.koalaresorthotels.com/");
+
+    cy.
+    contains("Log in").
+    click().
+    go("back").
+    go("forward").
+    go("back");
+
+    cy.
+    reload(true).
+    contains("Log in").
+    should("be.visible");
+  });
+});
