e99444a5f159deee17b1266114b0480db85c5ddc
ezrac0des <ezraapaydin@gmail.com>
2022-08-08 17:28:37 -0400

    first commit

---
+++ cypress/integration/com.ezra/day01/01firstTest.js
@@ -0,0 +1,13 @@
+//describe ('name', ()=> {test})
+describe("My First Test", () => {
+
+  //defining our test
+  //it('name', ()=> {test body})
+  it("URL Test", () => {
+
+    //go to google website
+    cy.visit("https://wwww.google.com/");
+
+    
+  });
+});
