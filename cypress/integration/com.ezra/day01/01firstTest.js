dc36c90a4e56432ce1ab3ca6f1009ec05d6187c7
ezrac0des <ezraapaydin@gmail.com>
2022-08-08 17:41:10 -0400

    sec

--- cypress/integration/com.ezra/day01/01firstTest.js
+++ cypress/integration/com.ezra/day01/01firstTest.js
@@ -1,13 +1,26 @@
-//describe ('name', ()=> {test})
 describe("My First Test", () => {
+  //describe ('name', ()=> {tests})
 
   //defining our test
-  //it('name', ()=> {test body})
   it("URL Test", () => {
+    //it('name', ()=> {test body})
 
-    //go to google website
+    //*visit(): to go to a website -> go to google website
     cy.visit("https://wwww.google.com/");
 
-    
+    //*url(): for testing url
+    //?should('include', 'text') -> does it include the given text?
+    cy.url().should("include", "google");
+    cy.url().should("include", "google", "com");
+    cy.url().should("include", "//");
+    cy.url().should("include", "");
+  });
+
+  it("Title Test", () => {
+    cy.visit("https://wwww.google.com/");
+
+    //*title(): for testing title
+    //?should('eq', 'text') -> is it equals to the given text?
+    cy.title().should("eq", "Google");
   });
 });
