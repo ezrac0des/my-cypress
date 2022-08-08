648f304052c36e5af0e5f8f2349da9e4ccf9873a
ezrac0des <ezraapaydin@gmail.com>
2022-08-08 17:51:36 -0400

    day01 end

---
+++ cypress/integration/com.ezra/day01/02loginTest.js
@@ -0,0 +1,14 @@
+describe("Crystal Key hotel", () => {
+  it("Test 1", () => {
+    cy.visit("https://qa-environment.koalaresorthotels.com/");
+
+    //login
+    //1. way: using open selector playground on the test page
+    //* get(): gets an element using css selector
+    cy.get("#navLogon > .nav-link").click();
+
+    //2. way: using text attribute of the element
+    //* contains(): gets an element using text attribute
+    cy.contains("Log in").click();
+  });
+});
