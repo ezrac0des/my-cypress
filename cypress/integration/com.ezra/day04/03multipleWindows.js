95e2f0ba3a35fbf4040821151752761952ef2237
ezrac0des <ezraapaydin@gmail.com>
2022-08-14 16:47:48 -0400

    drag and drop

--- cypress/integration/com.ezra/day04/03multipleWindows.js
+++ cypress/integration/com.ezra/day04/03multipleWindows.js
@@ -1,6 +1,24 @@
-describe('multiple windows', () => { 
+describe('multiple windows', () => {
+  it('removing target', () => {
+    cy.visit('https://the-internet.herokuapp.com/windows');
 
-    it('removing target', ()=>{
+    //* invoke('removeAttr', 'attributeName') -> removes the given attribute's value
+    cy.get('.example > a').invoke('removeAttr', 'target').click();
+  });
 
-    })
- })
+  //* only() -> runs only this test
+  it.only('opening in new tab', () => {
+    cy.visit('https://the-internet.herokuapp.com/windows');
+
+    cy.wait(3000);
+
+    //* then(() => {}) -> takes and holds element
+    //* prop('attribute') -> takes the value of given attribute
+    cy.get('.example > a').then((element) => {
+      const newURL = element.prop('href');
+      cy.visit(newURL);
+    });
+
+    cy.get('h3').should('have.text', 'New Window');
+  });
+});
---
+++ cypress/integration/com.ezra/day04/04dragAndDrop.js
@@ -0,0 +1,7 @@
+import '@4tw/cypress-drag-drop';
+
+describe('drag and drop', () => {
+  it('tit', () => {
+    cy.visit('https://www.globalsqa.com/demo-site/draganddrop/');
+  });
+});
