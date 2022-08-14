563f6c4eb88ab8ff74c138c1f6bd2a71e5951839
ezrac0des <ezraapaydin@gmail.com>
2022-08-14 13:21:15 -0400

    bitir cypress

--- cypress/integration/com.ezra/day03/03checkboxes.js
+++ cypress/integration/com.ezra/day03/03checkboxes.js
@@ -1,7 +1,14 @@
-describe('checkboxes', () => {
-    it('checking single box', ()=>{
-        cy.visit('http://automationpractice.com/index.php')
-    })
-})
+describe("checkboxes", () => {
+  it("checking single box", () => {
 
+    cy.visit("http://automationpractice.com/index.php");
+    
+    //* first() -> finds the first element that has the given css locator
+    cy.get(".sf-with-ul").first().click();
 
+    //* should('not.be.checked') -> asserts that the checkbox is not checked
+    cy.get('#layered_category_4').should('not.be.checked')
+
+    
+  });
+});
