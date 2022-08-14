f99f30a4c0ded413397497eebda0731a17c8998e
ezrac0des <ezraapaydin@gmail.com>
2022-08-14 16:12:38 -0400

    4

--- cypress/integration/com.ezra/day04/02iframe.js
+++ cypress/integration/com.ezra/day04/02iframe.js
@@ -1,5 +1,24 @@
+//! provides methods and codes to help cypress runner to contact with iframes
 import 'cypress-iframe';
 
-describe('iframes', () => { 
-    it('tit', )
- })
+describe('iframes', () => {
+  it('iframe', () => {
+    cy.visit('https://the-internet.herokuapp.com/iframe');
+
+    cy.get('h3').should('contain', 'Editor');
+
+    //* frameloaded('locator') -> locates and loads iframe
+    cy.frameLoaded('#mce_0_ifr');
+
+    //* iframe() -> enters into the iframe
+    //* find('htmlTag') -> finds the given HTML tag
+    //* clear() -> clears a textbox
+    cy.iframe().find('p').clear();
+
+    cy.iframe().find('p').type('Hello World!');
+
+    cy.get('.large-4 > div > a').should('contain.text', 'Elemental Selenium').click();
+
+    cy.title();
+  });
+});
---
+++ cypress/integration/com.ezra/day04/03multipleWindows.js
@@ -0,0 +1,6 @@
+describe('multiple windows', () => { 
+
+    it('removing target', ()=>{
+
+    })
+ })
