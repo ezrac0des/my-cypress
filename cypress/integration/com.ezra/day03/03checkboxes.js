3e69242d0f7b84a288746f4f2269a183f43b1379
ezrac0des <ezraapaydin@gmail.com>
2022-08-14 13:29:52 -0400

    hi

--- cypress/integration/com.ezra/day03/03checkboxes.js
+++ cypress/integration/com.ezra/day03/03checkboxes.js
@@ -1,13 +1,20 @@
 describe("checkboxes", () => {
   it("checking single box", () => {
-
     cy.visit("http://automationpractice.com/index.php");
-    
+
     //* first() -> finds the first element that has the given css locator
     cy.get(".sf-with-ul").first().click();
 
-    //* should('not.be.checked') -> asserts that the checkbox is not checked
-    cy.get('#layered_category_4').should('not.be.checked')
+    //* should('not.be.checked') -> asserts that the checkbox is not checked, returns true or false
+    cy.get("#layered_category_4").should("not.be.checked");
+
+    //* check() -> checks the given checkbox
+    cy.get("#layered_category_4").check();
+
+    //* should('be.checked') -> asserts that the checkox is checked, returns true or false
+    cy.get("#layered_category_4").shoul('be.checked')
+
+
 
     
   });
