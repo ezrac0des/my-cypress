95ba341f5b2cd7c11fa2fd42989879b535117695
ezrac0des <ezraapaydin@gmail.com>
2022-08-14 18:18:21 -0400

    a

--- cypress/integration/com.ezra/day04/01alerts.js
+++ cypress/integration/com.ezra/day04/01alerts.js
@@ -1,5 +1,5 @@
 describe('alerts', () => {
-  //TODO Cypress automatically accepts alerts
+  //? Cypress automatically accepts alerts
   it('alert test', () => {
     cy.visit('https://the-internet.herokuapp.com/javascript_alerts');
 
@@ -8,7 +8,6 @@ describe('alerts', () => {
     cy.get('#result').should('have.text', 'You successfully clicked an alert');
   });
 
-  //TODO Cypress automatically accepts alerts
   it('confirm alert', () => {
     cy.visit('https://the-internet.herokuapp.com/javascript_alerts');
 
@@ -20,26 +19,41 @@ describe('alerts', () => {
   it('dismiss alert', () => {
     cy.visit('https://the-internet.herokuapp.com/javascript_alerts');
 
-    cy.get(':nth-child(3) > button').click();
+    cy.get(':nth-child(2) > button').click();
 
     //* on('window:confirm', (param) => {}) -> does not confirm the alert, clicks cancel because of 'return false'
     cy.on('window:confirm', (str) => {
       return false;
     });
 
-    cy.get('#result').should('have.text', `You entered: Cancel`);
+    cy.get('#result').should('have.text', `You clicked: Cancel`);
   });
 
-  it.only('accept alert', () => {
+  it('accept alert', () => {
     cy.visit('https://the-internet.herokuapp.com/javascript_alerts');
 
-    cy.get(':nth-child(3) > button').click();
+    cy.get(':nth-child(2) > button').click();
 
     //* on('window:confirm', (param) => {}) -> confirms the alert, clicks ok because of 'return true'
-    cy.on('window:alert', (str) => {
+    cy.on('window:confirm', (str) => {
       return true;
     });
 
-    cy.get('#result').should('have.text', `You entered: Cancel`);
+    cy.get('#result').should('have.text', `You clicked: Ok`);
+  });
+
+  it('enter text to alert', () => {
+    cy.visit('https://the-internet.herokuapp.com/javascript_alerts');
+
+    //* window() -> checks pop up windows
+
+    cy.window()
+      //* then(($windowElement) => {}) -> enters data to prompt
+      .then(($windowElement) => {
+        cy.stub($windowElement, 'prompt').returns('Hi');
+        cy.get(':nth-child(3) > button').click();
+      });
+
+    cy.get('#result').should('have.text', `You entered: Hi`);
   });
 });
--- cypress/integration/com.ezra/day04/04dragAndDrop.js
+++ cypress/integration/com.ezra/day04/04dragAndDrop.js
@@ -1,4 +1,5 @@
-// import '@4tw/cypress-drag-drop';
+//TODO 1. npm install --save-dev @4tw/cypress-drag-drop in terminal
+import '@4tw/cypress-drag-drop';
 
 describe('drag and drop', () => {
   it('title', () => {
@@ -6,8 +7,28 @@ describe('drag and drop', () => {
 
     //* dblclick() -> double clicks
     cy.contains('Double-Click Me To See Alert').dblclick();
-    
+
+    //* on('window:alert', () =>{}) -> gets into the alert
+    cy.on('window:alert', (str) => {
+      //*  to.equal('text') -> checks if the text equals to the given text
+      expect(str).to.equal('You double clicked me.. Thank You..');
+
+      //*  to.eq('text') -> checks if the text equals to the given text
+      expect(str).to.eq('You double clicked me.. Thank You..');
+
+      //*  to.contain('text') -> checks if the text contains to the given text
+      expect(str).to.contain('You double clicked me..');
+    });
+
     //* rightclick() -> right clicks
     cy.get('.context-menu-one').rightclick();
+  });
+
+  it('drag and drop', () => {
+    cy.visit('http://demo.guru99.com/test/drag_drop.html');
+
+    //* drag('locator') -> drags the element to the gicen locator ({ force: true } may be needed in some cases)
+    cy.get(':nth-child(2) > .button') //? the element that will be dragged
+      .drag('#amt7', { force: true }); //? the place it should be dragged to
   });
 });
