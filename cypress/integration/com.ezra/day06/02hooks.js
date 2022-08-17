040b48f2201022146d4f697f4ce5d5d930043a73
ezrac0des <ezraapaydin@gmail.com>
2022-08-17 01:54:40 -0400

    a

---
+++ cypress/integration/com.ezra/day06/02hooks.js
@@ -0,0 +1,27 @@
+describe('hooks', () => {
+  //? runs first
+  before(() => {
+    console.log('before method');
+  });
+
+  //? runs second
+  beforeEach(() => {
+    console.log('before each method');
+    cy.visit('https://qa-environment.koalaresorthotels.com/');
+  });
+
+  //? runs second
+  after(() => {
+    console.log('after method');
+  });
+
+  //? runs first
+  afterEach(() => {
+    console.log('after each method');
+    cy.url().should('include', '/Account/Logon');
+  });
+
+  it('test', () => {
+    cy.contains('Log in').click();
+  });
+});
