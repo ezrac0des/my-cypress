774f3b5d1c8f68d66096d1d8249e57e533858a74
ezrac0des <ezraapaydin@gmail.com>
2022-08-18 22:02:41 -0400

    gitignore

---
+++ cypress/integration/smokeTest/01firstTest.js
@@ -0,0 +1,26 @@
+describe("My First Test", () => {
+  //describe ('name', ()=> {tests})
+
+  //defining our test
+  it("URL Test", () => {
+    //it('name', ()=> {test body})
+
+    //*visit(): to go to a website -> go to google website
+    cy.visit("https://www.google.com/");
+
+    //*url(): for testing url
+    //?should('include', 'text') -> does it include the given text?
+    cy.url().should("include", "google");
+    cy.url().should("include", "google", "com");
+    cy.url().should("include", "//");
+    cy.url().should("include", "");
+  });
+
+  it("Title Test", () => {
+    cy.visit("https://www.google.com/");
+
+    //*title(): for testing title
+    //?should('eq', 'text') -> is it equals to the given text?
+    cy.title().should("eq", "Google");
+  });
+});
